2c435b51fa05a9640bd8452fbb878ad901e6d7b9
Jayden Tucker <jaydenrtucker@gmail.com>
2020-11-28 20:48:02 -0800

    Added a paragraph component

--- website/src/App.js
+++ website/src/App.js
@@ -1,15 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import Paragraph from './components/Paragraph'
 import TitleCard from './components/TitleCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Paragraph
+          text='AYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy'
+        />
         <TitleCard 
           pageColour='#00FFFF'
           imageSrc='https://static.wikia.nocookie.net/terraria/images/f/f2/NPC_636.gif/revision/latest/scale-to-width-down/386?cb=20200618223104'
           title='Empress of Light'
+        />
+        <Paragraph
+          text='AYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy'
         />
       </header>
     </div>
---
+++ website/src/components/Paragraph.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./stylesheets/Paragraph.css";
+
+export default function Paragraph(props){
+  return (
+    <div className='paragraph'>
+      <p>{props.text}</p>
+    </div>
+  )
+}
