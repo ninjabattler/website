354b72d467c21ffe04465ec52b2061a7d34b21cc
Jayden Tucker <jaydenrtucker@gmail.com>
2020-11-28 21:10:54 -0800

    Added an article container class that will conain all the article components and changed some of the css on the image in the title card

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Paragraph from './components/Paragraph'
+import Paragraph from './components/Paragraph';
 import TitleCard from './components/TitleCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Paragraph
-          text='AYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy'
-        />
-        <TitleCard 
-          pageColour='#00FFFF'
-          imageSrc='https://static.wikia.nocookie.net/terraria/images/f/f2/NPC_636.gif/revision/latest/scale-to-width-down/386?cb=20200618223104'
-          title='Empress of Light'
-        />
-        <Paragraph
-          text='AYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy'
-        />
+        <article className='articleContainer'>
+          <Paragraph
+            text='AYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy'
+          />
+          <TitleCard 
+            pageColour='#FF4444'
+            imageSrc='https://static.wikia.nocookie.net/sonic/images/d/d4/Blackdoom_pose0_flatten.png/revision/latest/scale-to-width-down/1000?cb=20150410191238'
+            title='Black Doom'
+          />
+          <Paragraph
+            text='AYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYy'
+          />
+        </article>
       </header>
     </div>
   );
--- website/src/components/TitleCard.jsx
+++ website/src/components/TitleCard.jsx
@@ -6,11 +6,14 @@ export default function TitleCard(props){
     <div className='bossCard'>
       <div className='background2' style={{borderColor: props.pageColour, borderRightColor: "transparent"}}></div>
       <div className='background1'></div>
-      <h1 style={{textShadow: `2px 2px 0px ${props.pageColour}`}}>{props.title}</h1>
-      <img
-        src={props.imageSrc}
-        style={{filter: `drop-shadow(5px 5px 0px ${props.pageColour})`}}
+
+      <div className='imageContainer' style={{filter: `drop-shadow(5px 5px 0px ${props.pageColour})`}}> 
+        <img
+          src={props.imageSrc}
         />
+      </div>
+
+      <h1 style={{textShadow: `2px 2px 0px ${props.pageColour}`}}>{props.title}</h1>
     </div>
   )
 }
