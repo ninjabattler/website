edae074fc5ffcedf816ac990097dc96492aa2a49
Jayden Tucker <jaydenrtucker@gmail.com>
2021-01-13 18:51:07 -0800

    Added a build of the frontend to the backend

--- server/index.js
+++ server/index.js
@@ -5,19 +5,25 @@ const db = require('./db/db');
 const queries = require('./db/queries');
 const postRoutes = require('./routes/postRouter');
 const userRoutes = require('./routes/userRouter');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')))
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, 'build'));
+});
 
 db.connect()
 .then(()=>{
   console.log('YO')
 })
+.catch((err) => {
+  console.log(err)
+})
 app.use('/posts', postRoutes(db));
 app.use('/users', userRoutes(db));
 
 app.use(cors())
-
-app.get('/', (req, res)=>{
-  
-})
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
