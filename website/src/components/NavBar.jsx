802d17c4e0c2a652b48f393d75f2c598c1db6dda
Jayden Tucker <jaydenrtucker@gmail.com>
2021-01-14 19:46:20 -0800

    Added more responsive breakpoints for modile devices and added rickrolls

--- website/src/components/NavBar.jsx
+++ website/src/components/NavBar.jsx
@@ -6,10 +6,12 @@ import ParticlesBg from 'particles-bg';
 export default function NavBar(props){
   return (
     <div className='navBar'>
-      <img 
-        style={{filter: `drop-shadow(5px 5px 0px ${props.pageColour})`}}
-        src={image}
-      />
+      <a href='/'>
+        <img 
+          style={{filter: `drop-shadow(5px 5px 0px ${props.pageColour})`}}
+          src={image}
+        />
+      </a>
       <div className='navOptions'>
         <div className='option'>
           <div>
