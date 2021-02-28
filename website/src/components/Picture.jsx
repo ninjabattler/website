8d9be656ffb12035d8206d542c670b599f79a7a3
Jayden Tucker <jaydenrtucker@gmail.com>
2021-02-28 15:30:01 -0800

    Fixed image position on articles

--- website/src/components/Picture.jsx
+++ website/src/components/Picture.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import "./stylesheets/Picture.css";
 
-export default function Picture(props){
+export default function Picture(props) {
   return (
-    <img
-      className='picture'
-      src={props.imageSrc}
-      style={{filter: `drop-shadow(5px 5px 0px ${props.pageColour})`}}
-    />
+    <div className='picture'>
+      <img
+        src={props.imageSrc}
+        style={{ filter: `drop-shadow(5px 5px 0px ${props.pageColour})` }}
+      />
+    </div>
   )
 }
