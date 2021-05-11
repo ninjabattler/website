b5b774aab843cce4e1655a5420e1d38e1a65e5a1
Jayden Tucker <jaydenrtucker@gmail.com>
2021-05-10 21:44:50 -0700

    Updated the footer to remove the links

--- website/src/components/Footer.jsx
+++ website/src/components/Footer.jsx
@@ -4,13 +4,8 @@ import "./stylesheets/Footer.css";
 export default function Footer(props){
   return (
     <footer className='footer'>
-      <div className='linkDisplay'>
-      <a href='https://github.com/ninjabattler'>
-        <div><i class="fab fa-github"></i>https://github.com/ninjabattler</div>
-      </a>
-      <a href='https://www.linkedin.com/in/jayden-tucker-52a5711ba/'>
-        <div><i class="fab fa-linkedin"></i>https://www.linkedin.com/in/jayden-tucker-52a5711ba/</div>
-      </a>
+      <div className='topBar'>
+      
       </div>
     </footer>
   )
