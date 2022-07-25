439268a50c1cf20060c2378b02dcea326e093ead
Jayden Tucker <jaydenrtucker@gmail.com>
2022-07-24 18:08:33 -0700

    removed commented code in the review page

--- next/pages/articles/[title].jsx
+++ next/pages/articles/[title].jsx
@@ -110,30 +110,6 @@ export default function ArticlePage(props) {
               setIsDisliked={setIsDisliked}
             />
 
-            {/* <aside id={styles.shareBar}>
-              <div className="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-size="small">
-                <a target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(windowServer.location).replace(/'/g, "%27").replace(/"/g, "%22")}`} className="fb-xfbml-parse-ignore" rel="noreferrer">
-                  <Facebook className={styles.shareIcon} />
-                </a>
-              </div>
-              <div>
-                <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" data-show-count="false" rel="noreferrer">
-                  <Twitter className={styles.shareIcon} />
-                </a>
-                <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-              </div>
-              <div>
-                <a href={`https://www.linkedin.com/sharing/share-offsite/?url=${encodeURIComponent(windowServer.location).replace(/'/g, "%27").replace(/"/g, "%22")}`} target="_blank" rel="noreferrer">
-                  <LinkedIn className={styles.shareIcon} />
-                </a>
-              </div>
-              <div>
-                <a href={`http://www.reddit.com/submit?url=${windowServer.location}&title=Ninjabattler-${props.articleData.title}`} target="_blank" rel="noreferrer">
-                  <Reddit className={styles.shareIcon} />
-                </a>
-              </div>
-            </aside> */}
-
             <ShareBar
               title={props.articleData.title}
             />
