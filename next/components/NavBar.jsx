c29b9ce040537bb8cf284f19c0b953b2bd70f01f
Jayden Tucker <jaydenrtucker@gmail.com>
2022-07-26 16:27:33 -0700

    fixed the responsiveness of the navbar

--- next/components/NavBar.jsx
+++ next/components/NavBar.jsx
@@ -15,23 +15,6 @@ export default function NavBar(props) {
       {linkClicked && (<LoadingOverlay />)}
       <div className={styles.navBar}>
         <p onClick={() => { setLinkClicked(true) }} id={styles.shadow}></p>
-        {/* <div id={styles.navParticles}>
-          <ParticlesBg num={5} color='#252525' type="custom" bg={true} config={{
-          num: [2, 4],
-          rps: 0.1,
-          radius: [5, 40],
-          life: [1.5, 3],
-          v: [1, 2],
-          tha: [-40, 40],
-          alpha: [0.6, 0],
-          scale: [.1, 0.6],
-          position: "all",
-          color: ["#FFFF00", "#050843"],
-          cross: "dead",
-          // emitter: "follow",
-          random: 15
-        }} />
-        </div> */}
         <div className={styles.spaceBackground}></div>
         <Link href="/">
           <a onClick={() => { setLinkClicked(true) }}>
@@ -70,31 +53,6 @@ export default function NavBar(props) {
             </div>
           </div>
         </div>
-
-        {/* Drop down lists */}
-        {/* <div className={styles.navOptions} style={{ opacity: drop ? 1 : 0, height: 0 }}>
-          <div className={styles.option} style={{ opacity: '0', zIndex: -10000, height: 0 }}>
-            <div>
-            </div>
-          </div><div className={styles.option} style={{ opacity: '0', zIndex: -10000, height: 0 }}>
-            <div>
-            </div>
-          </div>
-          <div className={styles.option} onMouseEnter={() => { drop ? setDrop(true) : setDrop(false) }} onMouseLeave={() => { setDrop(false) }}>
-            {drop ?
-              (<div className={styles.socialLinks} style={{ textAlign: 'left', alignItems: 'flex-start', marginTop: drop ? "90px" : "-100px" }}>
-                <span><a onClick={() => { setLinkClicked(true) }} href='https://www.linkedin.com/in/jayden-tucker-52a5711ba/'><LinkedIn /> Linkedin</a></span>
-                <span><a onClick={() => { setLinkClicked(true) }} href='https://github.com/ninjabattler'><GitHub /> Github</a></span>
-              </div>)
-              :
-              (<></>)
-            }
-          </div>
-          <div className={styles.option} style={{ opacity: '0', zIndex: -10000, height: 0 }}>
-            <div>
-            </div>
-          </div>
-        </div> */}
       </div>
     </>
   )
