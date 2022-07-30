d45f598abd9120dbb0f1369a9fa4b744e0a72d43
Jayden Tucker <jaydenrtucker@gmail.com>
2022-07-30 12:20:38 -0700

    redesigned the dialouge component and fixed it's responsiveness

--- next/components/Dialogue.jsx
+++ next/components/Dialogue.jsx
@@ -3,14 +3,15 @@ import styles from "../styles/Dialogue.module.css";
 
 export default function Dialogue(props){
   return (
-    <div className={styles.dialogue} style={{ filter: `drop-shadow(2px 2px 0px ${props.pageColour})` }}>
+    <div className={styles.dialogue} style={{ filter: `drop-shadow(0px 0.3vw 0px ${props.pageColour})` }}>
       <div className={styles.dialogueText}>
-        <h3 style={{ filter: `drop-shadow(2px 2px 0px ${props.pageColour})` }}>
+        <h3>
           {props.speaker}
         </h3>
         <p>{props.text}</p>
       </div>
-      <img src={props.imageSrc} style={{ filter: `drop-shadow(2px 2px 0px ${props.pageColour})` }} />
+      <div className={styles.background1}></div>
+      <img src={props.imageSrc} />
     </div>
   )
 }
