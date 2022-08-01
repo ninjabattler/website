09ed275413d22d96f179c57d6299f92ebd6b037c
Jayden Tucker <jaydenrtucker@gmail.com>
2022-08-01 02:09:46 -0700

    started redesigning the home page and added a new  websiterobot.webp file

--- next/pages/index.jsx
+++ next/pages/index.jsx
@@ -9,45 +9,19 @@ const QUOTES = [
   'Welcome Chrome User'
 ]
 const SUB_QUOTES = [
-  'I hope'
+  'to the land of uneducated opinions and a few shades of gray!'
 ]
 
 let i = 0
 
 export default function Home(props) {
-
-  const quoteIndex = Math.floor(Math.random() * QUOTES.length)
+  const [quoteIndex, setQuoteIndex] = useState(Math.floor(Math.random() * QUOTES.length));
   const chosenQuote = QUOTES[quoteIndex]
   const chosenSubQuote = SUB_QUOTES[quoteIndex]
 
-  let [displayedQuote, setDisplayedQuote] = useState('|')
-  let [displayedSubQuote, setDisplayedSubQuote] = useState('|')
-
-
-  useEffect(() => {
-    const startQuoteAnim = setTimeout(() => {
-
-      const quoteAnim = setInterval(interval, 100)
-
-      function interval() {
-        setDisplayedQuote(chosenQuote.slice(0, i) + '|')
-        setDisplayedSubQuote(chosenSubQuote.slice(0, i) + '|')
-        i++
-
-
-        if (i >= chosenQuote.length) {
-          setDisplayedQuote(chosenQuote)
-          setDisplayedSubQuote(chosenSubQuote)
-          clearInterval(quoteAnim)
-        }
-      }
-
-    }, 800)
-  }, [])
-
   return (
     <>
-      <VideoBackground overlayColour pageColour="#404040" />
+      <VideoBackground overlayColour pageColour="#00FFFF" />
       <div className={styles.container}>
         <Head>
           <title>Ninjabattler</title>
@@ -65,7 +39,7 @@ export default function Home(props) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
           <meta name="google-site-verification" content="Cq6r13JSr-HUsKYp5H2wxCqR7tIK4SQGxMxodWHx1i4" />
-          
+
           {/* <!--FONTS--> */}
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Righteous&family=Gloria+Hallelujah&family=Trade+Winds&family=Hanalei+Fill&family=Rock+Salt&display=swap" rel="stylesheet" />
@@ -73,35 +47,15 @@ export default function Home(props) {
         <main id={styles.homePage}>
           <div id={styles.homePageBackground}></div>
 
-          <img id={styles.homePageRobot} src="/Website Robot 2.png" />
-          <div id={styles.homePageContainer}>
-            <h1>{displayedQuote}</h1>
-            <h4>{displayedSubQuote}</h4>
-          </div>
-
-          <p>Welcome to the land of uneducated opinions and a few shades of gray! Browsing this place, you will find incredible opinions on:</p>
-
-          {/* Video game */}
-          <div className={styles.homePageInfoDiv}>
-            <video autoPlay muted loop>
-              <source src='https://files.ninjabattler.ca/video/chaotix.mp4' type="video/mp4"></source>
-            </video>
-            <p>I&apos;ve invented the idea of writing words on my opinions on video games! Copyright 2021, do not steal.</p>
-          </div>
-
-          {/* Programming */}
-          <div className={`${styles.homePageInfoDiv} ${styles.homePageInfoDivFlipped}`}>
-            <p>I write code! Mayhaps I will write an article on the code I&apos;ve written every now and then!</p>
-            <CodeBlock code={`const Imap = require('imap');\nrequire('dotenv').config();\nconst imap = new Imap({\n  port: 993,\n  host: 'imap.gmail.com',\n  user: process.env.EMAIL,\n  password: process.env.PASSWORD,\n  tls: true\n});\nimap.once('ready', () => {\n  console.log('Imap is ready to hack Nasa');\n  imap.end();\n})\nimap.once('error', (err) => {\n  console.log(err);\n})\nimap.once('end', (err) => {\n  console.log('There is no Nasa');\n})\nimap.connect();`} language='js' />
-          </div>
-
-          {/* Stuff */}
-          <div className={styles.homePageInfoDiv}>
-            <video autoPlay muted loop>
-              <source src='/defaultVideo.mp4' type="video/mp4"></source>
-            </video>
-            <p>And whatever else I feel like writing about, any and all criticism is encouraged! The comments exist for a reason!</p>
-          </div>
+          <img id={styles.homePageRobot} src="/websiterobot.webp" />
+          <section id={styles.part1}>
+            <div id={styles.scrollToOne} />
+            <h1>{chosenQuote}</h1>
+            <h4>{chosenSubQuote}</h4>
+            <p>
+              Here in this small corner of the internet, you'll find lots of words, words written by me, if you can believe it. There's even pictures and videos, and some blender stuff. But what topics do these words speak of?
+            </p>
+          </section>
 
         </main>
       </div>
