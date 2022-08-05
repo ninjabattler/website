18440dff63408ab53e5649b9f7dd6e74b34457d8
Jayden Tucker <jaydenrtucker@gmail.com>
2022-08-04 20:23:18 -0700

    fixed the ip grabbing on the posts page

--- ssr/posts.js
+++ ssr/posts.js
@@ -1,10 +1,9 @@
-import axios from 'axios';
 import db from '../db/db';
+import requestIp from 'request-ip'
 import { selectAllPostsData, selectUserId, insertNewUser } from '../db/queries';
 
-export const postsServerSideProps = async () => {
-  let ip = await axios({ method: 'get', url: `https://api.ipify.org?format=json`, headers: { 'Content-Type': 'application/json' }, })
-  ip = ip.data.ip;
+export const postsServerSideProps = async (req) => {
+  const ip = requestIp.getClientIp(req.req)
 
   const postsArray = await selectAllPostsData(db)
   const userId = await selectUserId(db, ip)
