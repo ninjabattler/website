6cdf861c20c8d7f9068971726b7b8aca6534cc64
Jayden Tucker <jaydenrtucker@gmail.com>
2022-08-13 00:21:40 -0700

    added a moonlord option to the terrariaText, and tweaked the images on the title cards

--- components/animatedText/TerrariaText/TerrariaText.jsx
+++ components/animatedText/TerrariaText/TerrariaText.jsx
@@ -4,7 +4,7 @@ import styles from './TerrariaText.module.css'
 export default function TerrariaText(props) {
 
   return (
-    <span className={`${styles.terrariaText} ${props.draedon && styles.draedon} ${props.yharim && styles.yharim}`} style={props.colour && { color: props.colour }}>
+    <span className={`${styles.terrariaText} ${props.draedon && styles.draedon} ${props.moonlord && styles.moonlord} ${props.yharim && styles.yharim}`} style={props.colour && { color: props.colour }}>
       <b>{props.text}</b>
     </span>
   )
