9420945350884c4733f9977b8ba27fdbf97c8923
Jayden Tucker <jaydenrtucker@gmail.com>
2022-08-15 15:49:19 -0700

    converted the api routes to typescript

---
+++ pages/api/comments/newComment.ts
@@ -0,0 +1,25 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { QueryResult } from 'pg';
+import db from '../../../db/db';
+import insertNewComment from '../../../db/inserts/insertNewComment';
+import { CommentData, PostIdType, UserData, UserIdType } from '../../../types';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  const userId: UserIdType = Number(req.query.userId);
+  const postId: PostIdType = Number(req.query.postId);
+
+  await insertNewComment(db, req.query.content as string, postId, userId)
+    .then(async (response: CommentData) => {
+
+      const user: QueryResult<UserData> = await db.query(`
+      SELECT * FROM users WHERE id = $1
+      `, [userId])
+
+      res.status(200).json({
+        content: req.query.content,
+        data: response.date,
+        avatar: user.rows[0].avatar,
+        username: user.rows[0].username
+      })
+    })
+}
---
+++ pages/api/sitemapData.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import db from '../../db/db';
+import selectAllArticles from '../../db/selects/selectAllArticles';
+import { ArticleData, TitleType } from '../../types';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
+  await selectAllArticles(db)
+    .then(async (articles: ArticleData[]) => {
+      const articleTitles: TitleType[] = [];
+
+      articles.forEach(article => {
+        articleTitles.push(article.title.toLowerCase().replace(/ /g, '_'));
+      });
+
+      res.status(200).json({
+        articles: articleTitles
+      })
+    })
+}
