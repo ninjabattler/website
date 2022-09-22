f12aa3542099b63c806e26949f8adf37fcccb30b
Jayden Tucker <jaydenrtucker@gmail.com>
2022-09-21 23:30:35 -0700

    added a mobile version of the pixi background

--- components/PixiBackground/PixiBackground.tsx
+++ components/PixiBackground/PixiBackground.tsx
@@ -9,10 +9,11 @@ const PixiBackground: ComponentType<{}> = () => {
       height: window.innerHeight,
       width: window.innerWidth
     });
-
     document.getElementById(styles.pixiBackground).appendChild(app.view);
 
-    const spaceBackground = Sprite.from('homespace.png');
+    const isMobile = window.innerWidth <= 425;
+
+    const spaceBackground = Sprite.from(isMobile ? 'homespacemobile.png' : 'homespace.png');
     const spaceGlow = new BloomFilter(40);
     spaceBackground.filters = [spaceGlow];
     spaceBackground.x = app.view.width / 2;
@@ -20,7 +21,7 @@ const PixiBackground: ComponentType<{}> = () => {
     spaceBackground.anchor.x = 0.5;
     spaceBackground.anchor.y = 0.5;
 
-    const sun = Sprite.from('homesun.png');
+    const sun = Sprite.from(isMobile ? 'homesunmobile.png' : 'homesun.png');
     const sunRays = new GodrayFilter({
       alpha: 0.4,
       parallel: false,
@@ -31,10 +32,16 @@ const PixiBackground: ComponentType<{}> = () => {
     sun.filters = [spaceGlow, sunRays]
     sun.x = app.view.width / 2;
     sun.y = app.view.height / 2;
-    sun.width = window.innerWidth + (window.innerWidth / 10);
+    sun.width = window.innerWidth + (window.innerHeight / 10);
     sun.height = window.innerHeight + (window.innerHeight / 10);
     sun.anchor.x = 0.5;
     sun.anchor.y = 0.5;
+
+    if (isMobile) {
+      sun.width = window.innerWidth + (window.innerHeight * 0.15)
+
+      sunRays.center = [app.view.width, app.view.height / 10]
+    }
 
     app.stage.addChild(spaceBackground);
     app.stage.addChild(sun);
