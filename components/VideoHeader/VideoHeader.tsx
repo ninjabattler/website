62f5b31677f13ff53acb80fc84f35db02b1723dc
Jayden Tucker <jaydenrtucker@gmail.com>
2023-02-19 23:59:54 -0800

    added perspective to the video header

--- components/VideoHeader/VideoHeader.tsx
+++ components/VideoHeader/VideoHeader.tsx
@@ -11,7 +11,7 @@ type VideoHeaderProps = {
 }
 
 const VideoHeader: ComponentType<VideoHeaderProps> = ({ video, pageColour, title, infoBarProps }) => (
-  <div className={styles.videoHeader}>
+  <>
     <div id={styles.videoContainer}>
       {!video.startsWith('htt') && (<div id={styles.videoOverlay} style={{ backgroundColor: pageColour }}></div>)}
       <video loop muted autoPlay >
@@ -20,21 +20,22 @@ const VideoHeader: ComponentType<VideoHeaderProps> = ({ video, pageColour, title
         <source src={video ? video.replace('http://', 'https://') : "/defaultVideo.mp4"} type="video/mp4"></source>
       </video>
     </div>
+    <div className={styles.videoHeader}>
+      <div className={styles.bar2}></div>
+      <div className={styles.barC} style={{ backgroundColor: pageColour, borderRightColor: "transparent" }}></div>
+      <div className={`${styles.barC} ${styles.barGlow}`} style={{ backgroundColor: pageColour, borderRightColor: "transparent" }}></div>
+      <div className={styles.bar1}></div>
+      <div className={`${styles.bar1} ${styles.sketch}`}></div>
 
-    <div className={styles.bar2}></div>
-    <div className={styles.barC} style={{ backgroundColor: pageColour, borderRightColor: "transparent" }}></div>
-    <div className={`${styles.barC} ${styles.barGlow}`} style={{ backgroundColor: pageColour, borderRightColor: "transparent" }}></div>
-    <div className={styles.bar1}></div>
-    <div className={`${styles.bar1} ${styles.sketch}`}></div>
+      <h1>
+        {title}
+      </h1>
 
-    <h1>
-      {title}
-    </h1>
-
-    <div className={styles.infoContainer}>
-      <InfoBar category={infoBarProps.category} date={infoBarProps.date} genre={infoBarProps.genre} />
+      <div className={styles.infoContainer}>
+        <InfoBar category={infoBarProps.category} date={infoBarProps.date} genre={infoBarProps.genre} />
+      </div>
     </div>
-  </div>
+  </>
 )
 
 export default VideoHeader
