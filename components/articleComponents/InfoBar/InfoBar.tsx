d07f7dc56351b882a61cc4255760f378e8a00800
Jayden Tucker <jaydenrtucker@gmail.com>
2023-02-21 12:30:31 -0800

    added the typewriter effect to the info bar

--- components/articleComponents/InfoBar/InfoBar.tsx
+++ components/articleComponents/InfoBar/InfoBar.tsx
@@ -1,6 +1,8 @@
-import React, { ComponentType } from 'react';
+import React, { ComponentType, useEffect } from 'react';
 import styles from "./InfoBar.module.scss";
 import { formatSqlDate } from '../../../helpers/dateHelpers';
+import Typewriter from 'typewriter-effect/dist/core';
+import { TypewriterClass } from 'typewriter-effect';
 
 export type InfoBarProps = {
   date: string;
@@ -8,18 +10,32 @@ export type InfoBarProps = {
   genre: string;
 }
 
-const InfoBar: ComponentType<InfoBarProps> = ({ date, category, genre }) => (
-  <div className={styles.infoBar}>
-    <div className={styles.infoUnderline} />
-    <div className={`${styles.infoUnderline} ${styles.sketch}`} />
-    <span>
-      <div>{formatSqlDate(date)}</div>
-      <div>/</div>
-      <div>{category}</div>
-      <div>/</div>
-      <div>{genre}</div>
-    </span>
-  </div>
-)
+const InfoBar: ComponentType<InfoBarProps> = ({ date, category, genre }) => {
+  useEffect(() => {
+    const typewriter: TypewriterClass = new Typewriter('#info', {
+      cursor: '',
+      delay: 20
+    });
+
+    typewriter
+      .pauseFor(1800)
+      .typeString(`
+        <div>${formatSqlDate(date)}</div>
+        <div>/</div>
+        <div>${category}</div>
+        <div>/</div>
+        <div>${genre}</div>
+      `)
+      .start()
+  }, [])
+
+  return (
+    <div className={styles.infoBar}>
+      <div className={styles.infoUnderline} />
+      <div className={`${styles.infoUnderline} ${styles.sketch}`} />
+      <span id="info"></span>
+    </div>
+  )
+}
 
 export default InfoBar
