c7bbd3b307d2202b9bd3b25515bee1a341171e7e
Jayden Tucker <jaydenrtucker@gmail.com>
2023-02-21 02:01:47 -0800

    added some animation to the navbar and updated the footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,14 +1,21 @@
 import React, { ReactElement } from 'react';
 import styles from "./Footer.module.scss";
+import { Twitter, GitHub, Instagram } from '@material-ui/icons';
 
 export default function Footer(): ReactElement {
   return (
     <footer className={styles.footer}>
-      <div className={styles.topBar}>
+      <div className={styles.sketch} />
 
-      </div>
-      <section id={styles.copyrightSection}>
+      <div className={styles.topBar} />
+
+      <section id={styles.linksSection}>
         <img src={'/Ninja placeholder.png'} alt='logo' />
+        <div>
+          <a href='https://twitter.com/ninjabattler' target='_blank' rel='noreferrer'><Twitter /> Twitter</a>
+          <a href='https://www.instagram.com/ninjabattler/' target='_blank' rel='noreferrer'><Instagram /> Instagram</a>
+          <a href='https://github.com/ninjabattler' target='_blank' rel='noreferrer'><GitHub /> Github</a>
+        </div>
       </section>
     </footer>
   )
