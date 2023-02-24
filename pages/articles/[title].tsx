db75a5b1e3f34e177ecff90bfe92fc560a090581
Jayden Tucker <jaydenrtucker@gmail.com>
2023-02-24 01:13:59 -0800

    redesigned the title card and added a drop shadow to paragraphs

--- pages/articles/[title].tsx
+++ pages/articles/[title].tsx
@@ -131,7 +131,7 @@ export default function ArticlePage({ articleData, disliked, liked, randomQuoteI
 
           {/* Main Content */}
           <article className={styles.articleContainer} style={windowServer.innerWidth < 426 ? { boxShadow: `2px 2px 0px ${article.colour}` } : { boxShadow: `5px 5px 0px ${article.colour}` }}>
-            <div className={styles.sketchBackground} />
+            <div id='articleSketch' className={styles.sketchBackground} />
 
             {article.narration && (<iframe id={styles.adAurisIframe} src={`${article.narration}?color=${article.colour.split('#')[1]}`} style={{ border: 'none', height: '100px', width: '80%' }} ></iframe>)}
 
