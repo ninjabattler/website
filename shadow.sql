fd09b4325e87d7c99c392ed2e6e12490922d0049
Jayden Tucker <jaydenrtucker@gmail.com>
2023-04-13 15:48:56 -0700

    improved the title card on mobile

---
+++ shadow.sql
@@ -0,0 +1,302 @@
+INSERT INTO posts(title, thumbnail, video_header, review, colour, category, genre, description, narration, content)
+VALUES('Shadow Squadron - 32-bit Space Combat', 'https://files.ninjabattler.ca/image/shadowsquadron/thumbnail.webp', 'https://files.ninjabattler.ca/video/shadowsquadron.mp4', true, '#3F437C', '32X', 'Space Sim', 'While space games may not be uncommon, especially on the 32x, 3D games definitely are, but does Shadow Squadron have any substance to go with it''s style?', 'https://narrations.ad-auris.com/widget/ninjabattler/shadow-squadron---32-bit-space-combat', '[
+    {
+      "type": "Paragraph",
+      "content": [
+        "Space games are anything but uncommon, especially on the 32x. The addon launched with 3 of them, those being Cosmic Carnage, Doom, and Star Wars Arcade. With Star Wars Arcade being such a memorable game that you''ve probably played 1000 times over before, of course, they''d try to do it again, but better. This brings us to the 32x exclusive, Shadow Squadron."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "Shadow Squadron is one of the few games that try to take full advantage of the 32x''s power, being a fully 3D game, similarly to Metal Head. But as good as the 32x''s 3D graphics were, how about the gameplay? As mentioned before, it''s similar in gameplay to Star Wars Arcade, but is it better? It''s an impossible-to-answer question, but I''ll still attempt it anyways!"
+      ]
+    },
+    {
+      "type": "TitleCard",
+      "title": "Scramble Fighters!",
+      "imageSrc": ""
+    },
+    {
+      "type": "Picture",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/title.webp"
+    },
+    {
+      "type": "Quote",
+      "quote": "Total Freedom, Total Choice!!! Choose between two distinctly different ships and dive, roll, bank or loop, attacking targets at will... Total Control!!!",
+      "source": "Shadow Squadron, Game Box"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "As stated above, Shadow Squadron is a space combat game, similar to Star Wars Arcade or Star Fox. Every level involves taking out a list of targets, which includes smaller, harder-to-hit ships, and massive, powerful ships that take much longer to take out. The main thing that sets this game apart, however, is the ability to move around freely, rather than being stuck on rails. The controls are pretty good as well, being able to speed up, slow down, and rotate."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "The game allows you to choose between two different ships, Feather 1 & 2. Feather 1 is a pretty basic build, firing lasers while holding <strong>A</strong>,",
+        {
+          "type": "Picture",
+          "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/feather1.webp",
+          "width": "80%",
+          "float": "right"
+        },
+        " and being able to fire homing missiles by double-tapping <strong>A</strong>. It can also speed up with <strong>B</strong>, and even faster by double tapping it. Speeding up or firing homing missiles will use up energy and, if you''re out of energy, it will use the ship''s hp instead. It also has the ability to use a shield that costs energy... but doesn''t actually work. The feather 1''s energy and hp will refill after every mission."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "The Feather 2 works pretty differently. It has one energy bar for the entire game, which never refills. by taking from the energy instead.",
+        {
+          "type": "Picture",
+          "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/feather2.webp",
+          "width": "80%",
+          "float": "left"
+        },
+        " It fires powerful lasers that can destroy enemy attacks and can charge a more powerful laser by holding down <strong>A</strong>, which costs energy. It has the same speed-up abilities as the feather 1, which also costs energy. Unlike feather 1, the hp is not refilled for free at the end of the level, instead taking from the energy to refill itself."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "One of the game''s standout features is its enemies. The smaller ships are pretty easy to take out and don''t pose much of a threat. They will shoot at and, sometimes, attempt to ram the player. If you''re using the feather 1, they can be taken out with one homing missile each. The larger ships, however, these ones will fire larger, slow-moving lasers from everywhere on their body. They take much longer to destroy and need to be destroyed piece by piece. The entire ship doesn''t need to be destroyed thankfully, just the centrepiece of them. This feature makes it immensely satisfying to take out ships and is usually the goal of each mission. Speaking of the missions..."
+      ]
+    },
+    {
+      "type": "TitleCard",
+      "title": "This Means War",
+      "imageSrc": ""
+    },
+    {
+      "type": "Picture",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/takeoff.webp"
+    },
+    {
+      "type": "Quote",
+      "quote": "The Fleet Academy has a tradition of excellence that is unsurpassed, and you''re going to make sure it stays that way by being the first on the scene, and the last one standing when the dust clears.",
+      "source": "Shadow Squadron Manual"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "Starting the game up, we''re given the choice between ships. Once chosen, we''re treated to one of two animations of our ship going through a quick check, before being shot out into space. Once in space, we''re briefed on the mission, and given a map of all the enemies in the area, and the ones we need to take out. Honestly, a pretty cool way to start the game and transition between missions. The missions don''t have any names in-game, but they do in the manual it seems, the first one being:"
+      ]
+    },
+    {
+      "type": "SubtitleCard",
+      "title": "Graduation Day",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/delmer.webp"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        {
+          "type": "Picture",
+          "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/graduation.webp",
+          "width": "80%",
+          "float": "right"
+        },
+        "Starting off, we got an easy intro here. The map contains 12 smaller fighters, and 2 larger ships, the Delmer class. As stated before, if you''re using the feather 1, the fighters will be easy to take out with just one homing shot each. The Delmer ships take a bit longer to take out. It seems that only the center of the ship needs to be taken out to destroy it. I usually do this by attacking them from the back, taking out the thrusters, and getting a straight shot to the core of the ship."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "One thing you''ll notice while fighting, is how little feedback there is when attacking the larger ships. You have no idea how much hp the ship''s part has left, and it doesn''t really feel like you''re doing much damage. Either way, once they are destroyed, the explosions are pretty satisfying. Once the enemies are cleared out, it''s on to the next mission."
+      ]
+    },
+    {
+      "type": "SubtitleCard",
+      "title": "No Admittance",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/accelgate.webp"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        {
+          "type": "Picture",
+          "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/noadmittance.webp",
+          "width": "80%",
+          "float": "left"
+        },
+        "Mission 2 gives us a bit more variety than the first one. Along with one Delmer, we have some new targets to take out: the Balsark class, Booster ship, and Accel Gate. There are some fighters, but they don''t need to be taken out thankfully."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "The big obstacle of this level is the gate. You can''t go through it, and it will push you away if you try to. It also attacks like the other ships. While the concept is cool, the gate is pretty big and can take some time to take out, however, it is possible to rotate around the gate, avoiding its shots and dealing consistent damage."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "The rest of the enemies are easy enough to take out, the Balsark ships are basically just larger Delmers. The final ship to take out is the Booster ship... which just flies away if you get too close. It still counts towards finishing the mission, destroying it only gives you some bonus points."
+      ]
+    },
+    {
+      "type": "SubtitleCard",
+      "title": "Dodge ''Em",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/medelus.webp",
+      "higher": false
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        {
+          "type": "Picture",
+          "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/dodgeem.webp",
+          "width": "80%",
+          "float": "right"
+        },
+        "Moving onto the third mission, we now got asteroids everywhere. There''s also a new ship, the Medelus class, to take out, as well as 4 booster ships and 8 small fighters. The booster ships in this level don''t seem to run away, so you do need to fight them. Thankfully, they can''t fight back."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "The Medelus ships are where the game starts to get a bit annoying. The ship''s attacks move faster, making dodging more difficult, and giving less time to attack the ship itself. Regardless, they really are the only difficult part of this level. The small fighters and booster ships are pretty easy to take out."
+      ]
+    },
+    {
+      "type": "SubtitleCard",
+      "title": "Armada",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/carvelus.webp"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        {
+          "type": "Picture",
+          "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/armada.webp",
+          "width": "80%",
+          "float": "left"
+        },
+        "Remember every ship from the last few missions? No? Well, you will now! This mission contains 17 small fighters and 10 large ships, 4 Delmer, 3 Balsark, 2 Medelus and one new class, the Carvelus. Honestly, this is the level that made me switch to easy mode. It takes fucking forever to take out all the ships compared to the other levels and, if you die, you got to it all over again."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "There''s not too much else to say at this point, most of the level is taking out ships that you''ve already fought before. The new ship, Carvelus, is pretty tough though. Only the centre part needs to be destroyed, but if you focus only on it then you get bombarded by it, so I recommend taking it slow and taking out the wings first."
+      ]
+    },
+    {
+      "type": "SubtitleCard",
+      "title": "Ganeluza Grandslam",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/ganeluza.webp"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        {
+          "type": "Picture",
+          "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/grandslam.webp",
+          "width": "80%",
+          "float": "right"
+        },
+        "Did you like the Carvelus from the last level? Hope so because now you gotta take two of them out, <em>and</em> you spawn next to one. This level also contains the largest and most intimidating ship in the game, the Ganeluza. Same strategy as the last level basically."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "Once you''ve taken out all Carvelus''s, and preferably all the small fighters too, it''s time to take on the flagship of the enemy''s army. The outer hull of the ship takes no damage, and it doesn''t attack you either. The only way to destroy it is to fly into the front of it and attack its core until it''s dead, during which it will try to attack you. Couple hits and it''s gone. Surprisingly easy for the largest ship in the game."
+      ]
+    },
+    {
+      "type": "SubtitleCard",
+      "title": "G.L. Cannon",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/cannon.webp"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        {
+          "type": "Picture",
+          "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/glcannon.webp",
+          "width": "80%",
+          "float": "left"
+        },
+        "After all that, we''re finally on the final level, and things are getting interesting once again. The enemy''s ultimate weapon has been revealed, a giant sun laser thing. There''s also a time limit on this one, 3 minutes to defeat it. The laser starts with a ring of Slave Units floating around it, which can only be damaged by hitting the red core that faces the laser itself."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "Once the 8 units are taken down, the time limit resets to 2 minutes, and a new ring of units is released from the laser, revealing its weak points. The other units can be ignored, as well as the small fighters that surround it as well, just circle around the laser and take it out. Once it''s taken out, the level ends and the credits roll."
+      ]
+    },
+    {
+      "type": "Underline"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "Overall, the game gets pretty repetitive, but taking out the large ships is very satisfying. The last level is a pretty cool final challenge, however, the time limit is pretty annoying to deal with."
+      ]
+    },
+    {
+      "type": "TitleCard",
+      "title": "Command Strategies",
+      "imageSrc": ""
+    },
+    {
+      "type": "Picture",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/strategies.webp"
+    },
+    {
+      "type": "Quote",
+      "quote": "They''ve parked a nightmare in our back yard. They plan to use the Sun to power up a massive laser cannon aimed straight at our home world.",
+      "source": "Shadow Squadron Manual"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "So that''s the core of the game taken care of, but we''re not done yet. This game has a few other features to it that are worth mentioning, with the first being the trace option. Once you complete the game, or get a game over, a new option appears at the bottom of the main menu, which allows you to play back your entire run. It''s impressive to see, and the camera angles aren''t too bad, although some situations can be weird to watch, mainly any time that the player is flying straight for long periods of time."
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "There are a couple of other interesting options you can find in the... well... options menu. One that I''ve showcased in the screenshots of this article, is the ability to change the colours of the player and enemy ships. By default, feather 1 is blue, feather 2 is red and enemies are green, however, you can choose from a few different colours for each of them. Kinda neat feature. There''s also a model viewer as well, allowing you to take a close look at all the ship models in the game, with the colours you''ve chosen. Pretty cool indeedeth."
+      ]
+    },
+    {
+      "type": "Underline"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "Unfortunately, this is a 32X game, which means that there''s not much on the development of the game. There is one interesting fact about it though. In Japan, the game goes by the name Stellar Assault, and it''s also, somehow, got a sequel on the Sega Saturn, Stellar Assault SS, exclusively in Japan. This is something it shares with Tempo, which also got a sequel on the same console, Super Tempo."
+      ]
+    },
+    {
+      "type": "TitleCard",
+      "title": "Mission Accomplished?",
+      "imageSrc": ""
+    },
+    {
+      "type": "Picture",
+      "imageSrc": "https://files.ninjabattler.ca/image/shadowsquadron/ending.webp"
+    },
+    {
+      "type": "Quote",
+      "quote": "No recharge units are available for Feather 2. (We never promised you a rose garden....)",
+      "source": "Shadow Squadron Manual"
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "Thus ends Shadow Squadron. It''s pretty easy to compare it to Star Wars Arcade and Starfox, but it does stand out from each of them with its free roaming and massive ships to take out. However, like Star Wars, it gets pretty repetitive in its game-play. Overall, if nothing else, Shadow Squadron is at the very least, an impressive 32X game that stands out from the other games in its minuscule library. I''d definitely recommend giving it a go!"
+      ]
+    },
+    {
+      "type": "Paragraph",
+      "content": [
+        "What are your thoughts if you have any? Have you played Shadow Squadron? If not, have I gotten you to play it? Can I fit a third question into this paragraph? Leave a comment if you have any answers, or I''ll be upset, and you will be too."
+      ]
+    }
+  ]'::json);
