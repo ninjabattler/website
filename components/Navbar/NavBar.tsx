e46eff3e0b26cd3b4775005290a20f5189cc5298
Jayden Tucker <jaydenrtucker@gmail.com>
2023-09-10 16:57:16 -0700

    got started on revamping the navbar

--- components/Navbar/NavBar.tsx
+++ components/Navbar/NavBar.tsx
@@ -2,6 +2,7 @@ import React, { ComponentType } from 'react';
 import styles from "./NavBar.module.scss";
 import Link from 'next/link'
 import { ColourType } from '../../types';
+import { ClearAllSharp } from '@material-ui/icons';
 
 type NavBarProps = {
   setLinkClicked: Function;
@@ -11,39 +12,13 @@ type NavBarProps = {
 
 const NavBar: ComponentType<NavBarProps> = ({ setLinkClicked, pageColour, isArticlePage }) => (
   <div className={`${styles.navBar} ${isArticlePage ? styles.articlePage : ""}`}>
-    <div className={styles.spaceOverlay} />
+    {/* <div className={styles.spaceOverlay} /> */}
     <div className={styles.sketch} />
 
-    <Link href="/">
-      <a onClick={(e) => { e.preventDefault(); setLinkClicked("/") }}>
-
-        {
-          isArticlePage ?
-            (
-              <div
-                className={styles.logoOverlay}
-              />
-            )
-            :
-            (
-              <img
-                // style={{ filter: `drop-shadow(5px 5px 0px ${pageColour})` }}
-                src={isArticlePage ? '/bannerBW.webp' : '/banner.webp'}
-                alt='logo'
-              />
-            )
-        }
-      </a>
-    </Link>
-
     <div className={styles.navOptions}>
-      <Link href="/posts" ><a className={styles.option} onClick={(e) => { e.preventDefault(); setLinkClicked("/posts") }}>Posts</a></Link>
-
-      <Link href="/articles" ><a className={styles.option} onClick={(e) => { e.preventDefault(); setLinkClicked("/articles") }}>Articles</a></Link>
-
-      <a className={`${styles.option} ${styles.reverse}`} href='https://github.com/ninjabattler' target='_blank' rel="noreferrer">Github</a>
-
-      <Link href="/about" ><a className={`${styles.option} ${styles.reverse}`} onClick={(e) => { e.preventDefault(); setLinkClicked("/about") }}>About</a></Link>
+      <button>
+        <ClearAllSharp />
+      </button>
     </div>
   </div>
 )
