98852d033a77bcd3b665514f686f7c8e7e60635a
Jayden Tucker <jaydenrtucker@gmail.com>
2023-09-16 17:13:37 -0700

    upated the styling of the navbar and added an open/closed state to ir

--- components/Navbar/NavBar.tsx
+++ components/Navbar/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { ComponentType } from 'react';
+import React, { ComponentType, useCallback, useState } from 'react';
 import styles from "./NavBar.module.scss";
 import Link from 'next/link'
 import { ColourType } from '../../types';
@@ -10,17 +10,55 @@ type NavBarProps = {
   isArticlePage: Boolean;
 }
 
-const NavBar: ComponentType<NavBarProps> = ({ setLinkClicked, pageColour, isArticlePage }) => (
-  <div className={`${styles.navBar} ${isArticlePage ? styles.articlePage : ""}`}>
-    {/* <div className={styles.spaceOverlay} /> */}
-    <div className={styles.sketch} />
+/**
+ * Main navbar component for the site
+ * @author ninjabattler
+ * @prop setLinkClicked Function
+ */
+const NavBar: ComponentType<NavBarProps> = ({ setLinkClicked, pageColour, isArticlePage }) => {
+  const [open, setOpen] = useState<boolean>(false);
 
-    <div className={styles.navOptions}>
-      <button>
-        <ClearAllSharp />
-      </button>
+  const openMenu = useCallback(() => {
+    setOpen(!open);
+  }, [open])
+
+  return (
+    <div className={`${styles.navBar} ${isArticlePage ? styles.articlePage : ""} ${open ? styles.fullscreen : ""}`}>
+      <div className={styles.sketch} />
+      <div className={styles.space} />
+
+      <div className={styles.navOptions}>
+        <button onClick={openMenu}>
+          <ClearAllSharp />
+        </button>
+      </div>
+
+      <div className={styles.links}>
+        <img src='/banner.webp' />
+        
+        <Link href={`/posts`} >
+          <a onClick={(e) => { e.preventDefault(); setLinkClicked(`/posts`) }}>
+            Posts
+          </a>
+        </Link>
+        <Link href={`/articles`} >
+          <a onClick={(e) => { e.preventDefault(); setLinkClicked(`/articles`) }}>
+            Articles
+          </a>
+        </Link>
+        <Link href={`/about`} >
+          <a onClick={(e) => { e.preventDefault(); setLinkClicked(`/about`) }}>
+            About
+          </a>
+        </Link>
+      </div>
+
+      <div className={styles.profileBar}>
+        <img src='/userAvatars/Mask 4.png' alt='profile picture' />
+        <span>Unknown Ninja</span>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default NavBar
