c3e31b08e6382a488128917f9fa92b6b0aacf5e5
Jayden Tucker <jaydenrtucker@gmail.com>
2024-05-16 19:28:13 -0700

    added more post proccessing effects, and a pulsing effect to the sun on the posts page background

--- components/backgrounds/PostsPageBackground/PostsPageBackground.tsx
+++ components/backgrounds/PostsPageBackground/PostsPageBackground.tsx
@@ -17,12 +17,15 @@ import {
   Mesh,
 } from "three";
 import {
-  DepthOfFieldEffect,
+  BloomEffect,
+  ScanlineEffect,
   EffectComposer,
   EffectPass,
   GodRaysEffect,
   KernelSize,
   RenderPass,
+  BlendFunction,
+  NoiseEffect,
 } from "postprocessing";
 
 /**
@@ -79,31 +82,71 @@ const PostsPageBackground: FC<{}> = () => {
       const sunMap = new TextureLoader().load("/threeJs/posts/sun.png");
       const sunMaterial = new SpriteMaterial({ map: sunMap });
       const sun = new Sprite(sunMaterial);
+
+      const sun2Material = new SpriteMaterial({ map: sunMap });
+      const sun2 = new Sprite(sun2Material);
+
       sun.position.x = 4;
-      sun.scale.x = 6;
-      sun.scale.y = 6;
+      sun.scale.x = 5;
+      sun.scale.y = 5;
+
+      sun2.position.x = 4;
+      sun2.position.z = 0.01;
+      sun2.scale.x = 5;
+      sun2.scale.y = 5;
 
       scene.add(sun);
+      scene.add(sun2);
 
       // Post Processing Effects
-      const sunRays = new GodRaysEffect(camera, sun, {
+      const sunRays = new GodRaysEffect(camera, sun2, {
         height: 960,
         kernelSize: KernelSize.SMALL,
         density: 0.96,
         decay: 0.92,
         weight: 0.5,
-        exposure: 0.54,
+        exposure: 0.25,
         samples: 120,
         clampMax: 1,
-        blur: false,
       });
+
+      const bloom = new BloomEffect({
+        intensity: 3,
+        radius: 0.2,
+      });
+
+      const noise = new NoiseEffect({
+        blendFunction: BlendFunction.COLOR_DODGE,
+      });
+      noise.blendMode.opacity.value = 0.025;
+
+      const scanlines = new ScanlineEffect({
+        blendFunction: BlendFunction.MULTIPLY,
+        density: 1.0,
+      });
+      scanlines.blendMode.opacity.value = 0.2;
 
       const composer = new EffectComposer(renderer);
       composer.addPass(new RenderPass(scene, camera));
       composer.addPass(new EffectPass(camera, sunRays));
+      composer.addPass(new EffectPass(camera, bloom));
+      composer.addPass(new EffectPass(camera, noise));
+      composer.addPass(new EffectPass(camera, scanlines));
+
+      // Render
 
       const renderScene = () => {
         sunMaterial.rotation += 0.001;
+        sun2Material.rotation += 0.001;
+        sun.scale.x += 0.0075;
+        sun.scale.y += 0.0075;
+        sunMaterial.opacity -= 0.008;
+
+        if (sun.scale.x >= 6.2) {
+          sun.scale.x = 5;
+          sun.scale.y = 5;
+          sunMaterial.opacity = 1;
+        }
 
         renderer.render(scene, camera);
         composer.render();
