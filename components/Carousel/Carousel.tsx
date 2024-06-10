59b10371b8f0aee0120c3a9a96709749266f12d6
Jayden Tucker <jaydenrtucker@gmail.com>
2024-06-10 11:53:20 -0700

    moved the article cards on the articles pageto their own component

--- components/Carousel/Carousel.tsx
+++ components/Carousel/Carousel.tsx
@@ -7,6 +7,7 @@ import { EffectCoverflow } from "swiper/modules";
 import { ArticleData } from "../../types";
 import { formatSanityDate } from "../../helpers/dateHelpers";
 import { CommentSharp, ThumbsUpDownSharp } from "@mui/icons-material";
+import ArticleCard from "../ArticleCard/ArticleCard";
 
 type CarouselProps = {
   articles: ArticleData[];
@@ -38,46 +39,7 @@ const Carousel: FC<CarouselProps> = ({ articles }) => {
         {articles.map((article, i) => {
           return (
             <SwiperSlide key={i} className={styles.slide}>
-              <Link href={`/articles/${article.slug}`}>
-                <div className={styles.thumbnail}>
-                  <Image
-                    src={article.thumbnail.url}
-                    width={article.thumbnail.width}
-                    height={article.thumbnail.height}
-                    loading="lazy"
-                    placeholder="blur"
-                    blurDataURL={article.thumbnail.blur}
-                    alt={article.thumbnail.alt}
-                  />
-
-                  <div className={styles.stats}>
-                    <span>
-                      <ThumbsUpDownSharp />
-                    </span>
-
-                    <span>
-                      {/* @ts-ignore */}
-                      <CommentSharp /> {article.comments}
-                    </span>
-                  </div>
-                </div>
-
-                <div className={styles.articleInfo}>
-                  <div className={styles.likeDislikeBar}>
-                    <div
-                      className={styles.fillBar}
-                      style={{
-                        width: `${(article.likes / (article.likes + article.dislikes)) * 100}%`,
-                        backgroundImage: `linear-gradient(90deg, ${article.colors.primary.hex} 0%, ${article.colors.secondary.hex} 100%)`,
-                      }}
-                    />
-                  </div>
-                  <h1>{article.title}</h1>
-                  <h2>
-                    <em>{formatSanityDate(article.date)}</em>
-                  </h2>
-                </div>
-              </Link>
+              <ArticleCard article={article} />
             </SwiperSlide>
           );
         })}
